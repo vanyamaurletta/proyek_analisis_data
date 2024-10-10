904749577c7d23972c4010ed8442210cf00a21ef
Vanya Maurletta <vanyamaurletta@Vanyas-MacBook-Pro.local>
2024-10-10 12:06:10 +0700

    perubahan

--- dashboard/dashboardbikesharing.py
+++ dashboard/dashboardbikesharing.py
@@ -1,7 +1,7 @@
 # import pacakge
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
 sns.set(style='dark')
